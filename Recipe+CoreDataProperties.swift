d766b8d716b49ddfa67db8fc3cd33a907f3f3d2e
Jimmy Brown <jabro4@gmail.com>
2016-03-13 13:17:03 -0700

    Started CoreData part of the app.

---
+++ Recipe+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe+CoreDataProperties.swift
+//  recipez
+//
+//  Created by Jimmy Brown on 3/13/16.
+//  Copyright © 2016 Jimmy Brown. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Recipe {
+
+    @NSManaged var image: NSData?
+    @NSManaged var ingredients: String?
+    @NSManaged var steps: String?
+    @NSManaged var title: String?
+
+}
---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  recipez
+//
+//  Created by Jimmy Brown on 3/13/16.
+//  Copyright © 2016 Jimmy Brown. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
